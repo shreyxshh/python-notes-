1e3d30e14c9340d9b5a7a7305d578ec99f49a4a6
shreyxshh <shreyashrawat0501@gmail.com>
2026-05-15 18:58:59 +0530

    args fucntionality

---
+++ function_args.py
@@ -0,0 +1,21 @@
+'''
+during initialization of any function if we dont know the no of parameters to be passed 
+solution -> use args
+
+notation to create a args - (*variable_name)
+example:-
+args -> *shreyash
+args canbe used to take various inputs at a time and pass large no of parameters to function directly 
+'''
+
+def a(*a):
+    print(type(a))
+a
+
+def add(*a):
+    print(a)
+add(1,2,3,4,5,6)
+
+'''
+args is a tuple type of data type which stores a large number data in single variable 
+'''
